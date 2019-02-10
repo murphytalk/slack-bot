6d400b6fb69320e9340ef7e496b640b2a5a21ea5
Mu Lu <murphytalk@gmail.com>
2019-02-11 02:23:14 +0900

    move Anki stuff to its own module

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,10 @@
+import inspect
 import re
 import sys
 from commands.pinyin.wordpad import PinyinCardsGen
 from commands._scraper import Scraper
+from commands._anki import ANKI, ANKI2
+assert ANKI, ANKI2  # silence flake8 complaing module imported but not used
 
 
 class URL(object):
@@ -12,6 +15,9 @@ class URL(object):
 
 
 class PY(object):
+    """py <Chinese word>
+    Return pinyin of the given chinese word, along with traditional chinese (if available)
+    """
     py_gen = PinyinCardsGen()
 
     @staticmethod
@@ -23,10 +29,15 @@ class PY(object):
         return res
 
 
-class Anki(object):
-    pass
+class HELP(object):
+    """help [command]
+    Show a list of commands that I understand, or full command help if the command is specified.
+    """
+    commands = {c.__name__: c.__doc__.strip() for n, c in inspect.getmembers(sys.modules[__name__], inspect.isclass) if c.__doc__}
 
-
+    @staticmethod
+    def run(cmd):
+        return HELP.commands[cmd.upper()] if cmd and len(cmd) > 0 else '\n'.join([doc.split('\n')[0] for c, doc in HELP.commands.items()])
 
 
 matcher = re.compile('^([A-Za-z]+) *(.*)$')
@@ -39,5 +50,5 @@ def dispatch(msg):
         match_res = matcher.match(msg)
         if match_res:
             cmd = match_res.group(1).upper()
-            cls = getattr(sys.modules[__name__], cmd)
+            cls = getattr(sys.modules[__name__], cmd, None)
             return cls.run(match_res.group(2)) if cls else None
